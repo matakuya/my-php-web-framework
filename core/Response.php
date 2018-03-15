6312284664e49199476d22bf70f1fe836ba9c19a
matakuya <alabaster.427@gmail.com>
2018-03-15 22:03:47 +0900

    7.2.15

--- core/Response.php
+++ core/Response.php
@@ -8,5 +8,61 @@
  */
 class Response
 {
+    /**
+     * @var string
+     */
+    protected $content;
 
+    /**
+     * @var int
+     */
+    protected $status_code = 200;
+
+    /**
+     * @var string
+     */
+    protected $status_text = 'OK';
+
+    /**
+     * @var array
+     */
+    protected $http_headers = array();
+
+    public function send()
+    {
+        header('HTTP/1.1' . $this->status_code . ' ' . $this->status_text);
+
+        foreach ($this->http_headers as $name => $value) {
+            header($name . ': ' . $value);
+        }
+
+        echo $this->content;
+    }
+
+    /**
+     * @param $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @param $status_code
+     * @param string $status_text
+     */
+    public function setStatusCode($status_code, $status_text = '')
+    {
+        $this->status_code = $status_code;
+        $this->status_text = $status_text;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function setHttpHeader($name, $value)
+    {
+        $this->http_headers[$name] = $value;
+    }
 }
